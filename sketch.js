f706a46fcff04a7719959275a59433058e22930f
Neptuna-it <157400654+neptuna-it@users.noreply.github.com>
2024-11-20 10:52:34 +0100

    ci sono riuscuto

--- sketch.js
+++ sketch.js
@@ -1,13 +1,29 @@
 let molds = [];
 let d;
+let p5Instance;
+let currentScore = 0;
+
 function setup() {
-  createCanvas(500, 500);
+  p5Instance = this;
+  window.p5Instance = this; 
+
+  const canvasParent = document.querySelector('.canvas-container');
+  createCanvas(400, 400).parent(canvasParent);
+
   angleMode(DEGREES);
   d = pixelDensity();
-  let num = window.num;
-  initializeMolds(num);
+  
+  currentScore = parseInt(document.getElementById("score").innerHTML, 10);
+  initializeMolds(currentScore);
 }
+
 function draw() {
+  const newScore = parseInt(document.getElementById("score").innerHTML, 10);
+  if (newScore !== currentScore) {
+    currentScore = newScore;
+    initializeMolds(currentScore);
+  }
+
   background(0, 5);
   loadPixels();
   for (let i = 0; i < molds.length; i++) {
@@ -15,30 +31,30 @@ function draw() {
     molds[i].display();
   }
 }
+
 function initializeMolds(num) {
   molds = [];
   for (let i = 0; i < num; i++) {
     molds.push(new Mold());
   }
 }
-function updateMolds(newScore) {
-  initializeMolds(newScore);
-}
+
 class Mold {
   constructor() {
     this.x = random(width / 2 - 20, width / 2 + 20);
     this.y = random(height / 2 - 20, height / 2 + 20);
-    this.r = 0.5;
+    this.r = random(0.1, 0.7);
     this.heading = random(360);
     this.vx = cos(this.heading);
     this.vy = sin(this.heading);
-    this.rotAngle = 45;
+    this.rotAngle = random(30, 60);
     this.rSensorPos = createVector(0, 0);
     this.lSensorPos = createVector(0, 0);
     this.fSensorPos = createVector(0, 0);
-    this.sensorAngle = 45;
-    this.sensorDist = 10;
+    this.sensorAngle = random(35, 55);
+    this.sensorDist = random(8, 12);
   }
+
   update() {
     this.vx = cos(this.heading);
     this.vy = sin(this.heading);
@@ -47,6 +63,7 @@ class Mold {
     this.getSensorPos(this.rSensorPos, this.heading + this.sensorAngle);
     this.getSensorPos(this.lSensorPos, this.heading - this.sensorAngle);
     this.getSensorPos(this.fSensorPos, this.heading);
+    
     let index, l, r, f;
     index = 4 * (d * floor(this.rSensorPos.y)) * (d * width) + 4 * (d * floor(this.rSensorPos.x));
     r = pixels[index];
@@ -54,6 +71,7 @@ class Mold {
     l = pixels[index];
     index = 4 * (d * floor(this.fSensorPos.y)) * (d * width) + 4 * (d * floor(this.fSensorPos.x));
     f = pixels[index];
+    
     if (f > l && f > r) {
       this.heading += 0;
     } else if (f < l && f < r) {
@@ -68,11 +86,13 @@ class Mold {
       this.heading += this.rotAngle;
     }
   }
+
   display() {
     noStroke();
     fill(255);
     ellipse(this.x, this.y, this.r * 2, this.r * 2);
   }
+
   getSensorPos(sensor, angle) {
     sensor.x = (this.x + this.sensorDist * cos(angle) + width) % width;
     sensor.y = (this.y + this.sensorDist * sin(angle) + height) % height;
